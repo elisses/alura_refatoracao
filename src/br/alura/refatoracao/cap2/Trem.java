b845d83cd6ab4c346ffb25c4523ae439aa95e0c5
elisses <fernandalisses@gmail.com>
2017-08-14 00:29:59 -0300

    Commit Inicial

---
+++ src/br/alura/refatoracao/cap2/Trem.java
@@ -0,0 +1,24 @@
+package br.alura.refatoracao.cap2;
+
+import java.util.List;
+
+public class Trem {
+
+	private List<Vagao> vagoes;
+	private int capacidade;
+	
+	public boolean podeReservar(int lugaresAReservar) {		
+		
+		int lugaresDisponiveis = capacidade - lugaresJaReservados();
+		return lugaresDisponiveis > lugaresAReservar; 
+	}
+
+	private int lugaresJaReservados() {
+		int lugaresJaReservados = 0;
+		for(Vagao vagao : vagoes) {
+			lugaresJaReservados += vagao.reservados();
+		}
+		return lugaresJaReservados;
+	}
+	
+}
